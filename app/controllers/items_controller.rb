a395bfce01fbc62074776d0160f7874e0c509f9b
Albert Byrone <albertbyrone1677@gmail.com>
2022-06-24 08:50:41 +0300

    commit tree f67443d0df78c7319084b8eda3bca6deacdcfb72

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,47 @@
 class ItemsController < ApplicationController
+  rescue_from  ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = find_item
+    render json: item
+  end
+
+  def create
+    user = find_user
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def find_item
+    Item.find_by(params[:id])
+  end
+
+  def find_user
+    User.find_by(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: :not_found
+  end
+
+  def items_params
+    params.permits(:name, :description, :price)
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
+  end
 end
+
+
+
